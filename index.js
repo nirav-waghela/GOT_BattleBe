ef4d9dd0692d28e244bcfb41925a963e7d1829d5
Ronak Ruparel <rr9323@gmail.com>
2020-05-15 16:27:39 +0530

    Revert "Update index.js"
    This reverts commit f1a69944dbf8c95581837f524d112beb19ff7040.

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const express = require("express");
+const express = require('express')
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const gotData = require("./models/got");
-const app_config = require("./configurations/app_config.js");
+const gotData = require('./models/got')
+const app_config = require('./configurations/app_config.js')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
 
-// mongoose
-//     .connect(
-//         app_config.database.connect_uri,
-//         {
-//             useUnifiedTopology: true,
-//             useNewUrlParser:true
-//         }
+mongoose
+    .connect(
+        app_config.database.connect_uri,
+        {   
+            useUnifiedTopology: true,
+            useNewUrlParser:true
+        }
 
-//     )
-//     .then(()=>{
-//         console.log('Database connection successful')
-//     })
-//     .catch(()=>{
-//         console.error('Database connection failed')
-//     })
+    )
+    .then(()=>{
+        console.log('Database connection successful')
+    })
+    .catch(()=>{
+        console.error('Database connection failed')
+    })
 
-// app.use(require('./routes/api'))
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
-});
+app.use(require('./routes/api'))    
+
+app.listen(3000, ()=>{
+    console.log('App listening on port 3000')
+})
+
+app.get('/',(req, res)=>{
+    res.send('hello')
+})
