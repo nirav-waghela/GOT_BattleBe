e90b0c5d5ed37a7da2f516553085914b787b1194
nirav-waghela <nirav.waghela000@gmail.com>
2020-05-15 20:42:34 +0530

    Update app_config.js

--- configurations/app_config.js
+++ configurations/app_config.js
@@ -5,7 +5,7 @@ const dbPassword = 'YDKYqWxbkiR3eGYt'
 let config = {
     port:8000,
     database: {
-        connect_uri: 'mongodb+srv://nirav:pEOw5P9aaU6d8gTo@cluster0-u0yh0.mongodb.net/got_battle?retryWrites=true&w=majority'
+        connect_uri: process.env.dbURL
         //mongodb+srv://nirav:YDKYqWxbkiR3eGYt@cluster0-u0yh0.mongodb.net/test?retryWrites=true&w=majority
         // `mongodb+srv://${dbUser}:${dbPassword}@cluster0-u0yh0.mongodb.net/test?retryWrites=true&w=majority`
     }
