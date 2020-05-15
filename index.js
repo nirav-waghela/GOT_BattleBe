e7dfd81bbf9530fdef2fb9c209469fffe03a407a
nirav-waghela <nirav.waghela000@gmail.com>
2020-05-15 21:04:30 +0530

    test deploy

--- index.js
+++ index.js
@@ -1,39 +1,87 @@
-const express = require('express')
+const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const gotData = require('./models/got')
-const app_config = require('./configurations/app_config.js')
+// const mongoose = require("mongoose");
+// const gotData = require("./models/got");
+// const app_config = require("./configurations/app_config.js");
+// const PORT = process.env.PORT;
+const app = express();
 
-const app = express()
-
-app.use(cors())
-app.use(bodyParser.json())
-
-
-mongoose
-    .connect(
-        app_config.database.connect_uri,
-        {   
-            useUnifiedTopology: true,
-            useNewUrlParser:true
-        }
-
-    )
-    .then(()=>{
-        console.log('Database connection successful')
-    })
-    .catch(()=>{
-        console.error('Database connection failed')
-    })
+app.use(cors());
+app.use(bodyParser.json());
 
 
-app.use(require('./routes/api'))    
+// mongoose
+//   .connect(app_config.database.connect_uri, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   })
+//   .then(() => {
+//     console.log("Database connection successful");
+//   })
+//   .catch(() => {
+//     console.error("Database connection failed");
+//   });
 
-app.listen(3000, ()=>{
-    console.log('App listening on port 3000')
-})
+// app.use(require("./routes/api"));
 
-app.get('/',(req, res)=>{
-    res.send('hello')
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const gotData = require('./models/got')
+// const app_config = require('./configurations/app_config.js')
+
+// const app = express()
+
+// app.use(cors())
+// app.use(bodyParser.json())
+
+
+// mongoose
+//     .connect(
+//         app_config.database.connect_uri,
+//         {   
+//             useUnifiedTopology: true,
+//             useNewUrlParser:true
+//         }
+
+//     )
+//     .then(()=>{
+//         console.log('Database connection successful')
+//     })
+//     .catch(()=>{
+//         console.error('Database connection failed')
+//     })
+
+
+// app.use(require('./routes/api'))    
+
+// app.listen(3000, ()=>{
+//     console.log('App listening on port 3000')
+// })
+
+// app.get('/',(req, res)=>{
+//     res.send('hello')
+// })
