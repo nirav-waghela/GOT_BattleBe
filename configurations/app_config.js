7fdc91a843124d15fba9a0757bc0f0b5dc9138f0
nirav-waghela <nirav.waghela000@gmail.com>
2020-05-17 12:30:50 +0530

    config changes

--- configurations/app_config.js
+++ configurations/app_config.js
@@ -1,11 +1,11 @@
 const dbUser = 'nirav'
-const dbPassword = 'YDKYqWxbkiR3eGYt'
+const dbPassword = 'pEOw5P9aaU6d8gTo'
 
 
 let config = {
     port:8000,
     database: {
-        connect_uri: 'mongodb+srv://nirav:pEOw5P9aaU6d8gTo@cluster0-u0yh0.mongodb.net/got_battle?retryWrites=true&w=majority'
+        connect_uri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-u0yh0.mongodb.net/got_battle?retryWrites=true&w=majority`
         //mongodb+srv://nirav:YDKYqWxbkiR3eGYt@cluster0-u0yh0.mongodb.net/test?retryWrites=true&w=majority
         // `mongodb+srv://${dbUser}:${dbPassword}@cluster0-u0yh0.mongodb.net/test?retryWrites=true&w=majority`
     }
